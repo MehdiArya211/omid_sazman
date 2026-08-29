0a6f3fadee57053a6ec415571e34e84a8820ddd0
Mehdi Aryanezhad <154962752+MehdiArya211@users.noreply.github.com>
2026-08-29 09:11:11 +0330

    docs: organize and document solution methods

--- VisitorManagment.Core/Services/Interfaces/IChartService.cs
+++ VisitorManagment.Core/Services/Interfaces/IChartService.cs
@@ -9,13 +9,22 @@ namespace VisitorManagment.Core.Services.Interfaces
 {
     public interface IChartService
     {
+        /// <summary>
+        /// اطلاعات موردنیاز برای نمایش را آماده می‌کند.
+        /// </summary>
         public Chart ShowReportOne();
        // public Chart ShowReport();
+        /// <summary>
+        /// اطلاعات موردنیاز برای نمایش را آماده می‌کند.
+        /// </summary>
         public ChartBarDto ShowReportBarChartSearch(DateTime? startDateSearch, DateTime? endDateSearch,
             int requestSubjectId = 0, int filterGharargahId = 0, int filterYeganId = 0);
 
 
         #region میزان فعالیت فرماندهان
+        /// <summary>
+        /// اطلاعات موردنیاز برای نمایش را آماده می‌کند.
+        /// </summary>
         public ChartFarmandehActivityDto ShowReportChartFarmandehActivity(string personCode);
         #endregion
 
