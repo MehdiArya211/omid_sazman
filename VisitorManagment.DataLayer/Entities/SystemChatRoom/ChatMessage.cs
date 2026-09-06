8f7c35e35c0ad8f9366cf0a30dfe2e90a0343138
Mehdi Aryanezhad <154962752+MehdiArya211@users.noreply.github.com>
2026-09-06 08:27:10 +0330

    Add professional realtime chat features

--- VisitorManagment.DataLayer/Entities/SystemChatRoom/ChatMessage.cs
+++ VisitorManagment.DataLayer/Entities/SystemChatRoom/ChatMessage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace VisitorManagment.DataLayer.Entities.SystemChatRoom
 {
@@ -15,6 +16,17 @@ namespace VisitorManagment.DataLayer.Entities.SystemChatRoom
         public Guid? ReplyToMessageId { get; set; }
         public string ReplyToSender { get; set; }
         public string ReplyToMessage { get; set; }
+        public DateTime? EditedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? ReadAt { get; set; }
+        public bool IsDelivered { get; set; }
+        public DateTime? DeliveredAt { get; set; }
+        [MaxLength(500)] public string AttachmentUrl { get; set; }
+        [MaxLength(260)] public string AttachmentName { get; set; }
+        [MaxLength(150)] public string AttachmentContentType { get; set; }
+        public long? AttachmentSize { get; set; }
         public ChatRoom ChatRoom { get; set; }
         public Guid ChatRoomId { get; set; }
     }
